242150acbe53b029e2102ddfd3cb6ce1e0c3afca
Kira Clements <kiwi.klc@gmail.com>
2023-01-15 14:23:44 -0700

    part 5 finished

--- starter/part-5/part_5.py
+++ starter/part-5/part_5.py
@@ -1,138 +1,7 @@
-#### Step 1 - Store data in a .txt
 
-### This step's instructions explains how to use the open() function, to write and read info from a .txt file. Follow the instructions to create and 
-## call a function to add a book, based off of the previous dictionaries for our library, to the .txt file properly formatted with commas as separators.
+# 1 EXIT
 
-# Code here
-
-# with open('library.txt','a') as file:
-#     file.write('FableHaven2, BradonMull, 2011, 4.2, 402\n')
-
-#### Step 2 - Read data from a .txt
-
-### Now take your previously create function which prints info about all the books in your library, but gets the info from a list, 
-## and make it work by reading the information in your home library's .txt document. This will take some new logic, but you can do it.
-
-# Code here
-
-### Step 3 - if __name__ == "__main__":
-
-## Wrap your main menu function call in an "if __name__ == '__main__':" statement to ensure it doesn't accidentally run if this file is imported as a module elsewhere.
-
-# Code this at the bottom of the script
-
-### Step 4 - Expand and refactor
-
-## Now follow the instructions in this final step. Expand your project. Clean up the code. Make your application functional. Great job getting your first Python 
-# application finished!
-
-# THIS WORKS!!!!
-
-# def get_book_dictionary():
-#     with open('library.txt', 'r') as file:
-#         file = file.readlines()
-
-#         for line in file:
-#             title, author, pages, year, rating = line.split(',')
-
-#             book_dictionary = {
-#                 'title': title,
-#                 'author': author,
-#                 'pages': int(pages),
-#                 'year': int(year),
-#                 'rating': float(rating)
-#             }
-
-#             print(book_dictionary)
-#             return book_dictionary
-
-
-#BOOK INFO
-
-def get_book_list():
-    with open('library.txt', 'r') as file:
-        file = file.readlines()
-        num = 0
-        book_list = {}
-        for line in file:
-            title, author, pages, year, rating = line.split(',')
-
-            book_dictionary = {
-                'title': title,
-                'author': author,
-                'pages': int(pages),
-                'year': int(year),
-                'rating': float(rating)
-            }
-
-            num += 1
-
-            book_list[num] = book_dictionary
-        
-        return book_list
-
-
-# get_book_list()
-
-
-
-def get_book_list_title_author(dictionary):
-    for value in dictionary.values():
-        print(value['title'] + ' by' + value['author'])
-
-def get_book_title(dictionary):
-       for value in dictionary.values():
-        print(value['title'])
-
-
-
-
-# 4 BOOK TITLE SEARCH
-
-def book_title_input():
-    search = input("please enter key letters or words of the title your looking for ")
-    return search
-
-
-
-def book_title_search(string, books):
-    for value in books.values():
-        bk = value['title'].lower()
-        if string in bk:
-            print(f'found the following matching results: {bk}')
-
-# 5 BOOK AUTHOR SEARCH
-
-def book_author_input():
-    search = input("please enter key letters, or name of the author your looking for ")
-    return search
-
-
-
-def book_author_search(string, books):
-    for value in books.values():
-        bk = value['author'].lower()
-        if string in bk:
-            print(f'found the following matching results: {bk}')
-
-
-# 6 GET INFO
-
-def book_info_input():
-    search = input("please enter the title of the book you want information of: ")
-    return search
-
-def book_info_search(string, books):
-    for value in books.values():
-        bk = value['title'].lower()
-        t = value['title']
-        a = value['author']
-        y = value['year']
-        r = value['rating']
-        p = value['pages']
-        if string == bk:
-            print(f"{t} is a book written by{a} that contains {p} pages, has a rating of {r}, and was written in {y}.")
-
+# Exit functionality inside of the menu.
 
 # 2 ADD A BOOK 
 
@@ -169,6 +38,88 @@ def new_book_input():
         file.write(f'{title}, {author}, {pages}, {year}, {rating}\n')
 
     return book_dictionary
+
+
+# 3 GET LIBRARY
+
+def get_book_list():
+    with open('library.txt', 'r') as file:
+        file = file.readlines()
+        num = 0
+        book_list = {}
+        for line in file:
+            title, author, pages, year, rating = line.split(',')
+
+            book_dictionary = {
+                'title': title,
+                'author': author,
+                'pages': int(pages),
+                'year': int(year),
+                'rating': float(rating)
+            }
+
+            num += 1
+
+            book_list[num] = book_dictionary
+        
+        return book_list
+
+
+def get_book_list_title_author(dictionary):
+    for value in dictionary.values():
+        print(value['title'] + ' by' + value['author'])
+
+def get_book_title(dictionary):
+       for value in dictionary.values():
+        print(value['title'])
+
+
+# 4 BOOK TITLE SEARCH
+
+def book_title_input():
+    search = input("please enter key letters or words of the title your looking for ")
+    return search
+
+
+
+def book_title_search(string, books):
+    for value in books.values():
+        bk = value['title'].lower()
+        if string in bk:
+            print(f'found the following matching results: {bk}')
+
+# 5 BOOK AUTHOR SEARCH
+
+def book_author_input():
+    search = input("please enter key letters, or name of the author your looking for ")
+    return search
+
+
+
+def book_author_search(string, books):
+    for value in books.values():
+        bk = value['author'].lower()
+        if string in bk:
+            print(f'found the following matching results: {bk}')
+
+
+# 6 GET INFO
+
+def book_info_input():
+    search = input("please enter the title of the book you want information of: ").lower()
+    return search
+
+def book_info_search(string, books):
+    for value in books.values():
+        bk = value['title'].lower()
+        t = value['title']
+        a = value['author']
+        y = value['year']
+        r = value['rating']
+        p = value['pages']
+        if string == bk:
+            print(f"{t} is a book written by{a} that contains {p} pages, has a rating of {r}, and was written in {y}.")
+
 
 
 # MAIN MENU
